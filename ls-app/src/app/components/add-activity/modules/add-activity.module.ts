298e53f453b38f0b9b569c0f70d1249e7af6200f
CEDMD\crme008 <marina.burlacu@iongroup.com>
2023-03-01 11:02:53 +0000

    create add-activity component

---
+++ ls-app/src/app/components/add-activity/modules/add-activity.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { AddActivityComponent } from "../add-activity.component";
+
+
+@NgModule ({
+    imports : [
+        CommonModule
+  
+    ],
+    declarations: [
+      AddActivityComponent
+    
+    ],
+    providers:[]
+})
+
+export class LoginModule { }
